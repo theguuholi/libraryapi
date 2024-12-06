bad7018ee766d2e37ffc417226109a3e65145ff8
Gus <g.92oliveira@gmail.com>
2024-12-06 09:06:32 -0300

    sql

---
+++ src/main/java/com/example/libraryapi/model/Autor.java
@@ -0,0 +1,36 @@
+package com.example.libraryapi.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "autor", schema = "public") // modificar o nome da table
+@Data
+public class Autor {
+    @Id
+    // @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "data_nascimento", nullable = false)
+    private LocalDate dataNascimento;
+
+    @Column(name = "nacionalidade", length = 50, nullable = false)
+    private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
+}
---
+++ src/main/java/com/example/libraryapi/model/GeneroLivro.java
@@ -0,0 +1,19 @@
+package com.example.libraryapi.model;
+
+public enum GeneroLivro {
+    FANTASIA,
+    FICCAO,
+    TERROR,
+    ROMANCE,
+    DRAMA,
+    SUSPENSE,
+    AVENTURA,
+    BIOGRAFIA,
+    AUTOAJUDA,
+    INFANTIL,
+    RELIGIAO,
+    HISTORIA,
+    POESIA,
+    QUADRINHOS,
+    OUTROS
+}
